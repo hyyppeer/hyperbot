6141710f98cf2f5e2a84e62a0fa4d8a2ca7f1028
Mi2050 <95050519+Mi2050@users.noreply.github.com>
2023-06-17 01:55:52 +0200

    add logging level to logger

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,14 @@ import { Cli } from './bot/services/cli';
 import { Dashboard } from './bot/services/dashboard/dashboard';
 import { Shell } from './bot/services/town/shell';
 import { readBundle, readConfig } from './util/config';
-import { Logger } from './util/logger';
+import { Logger, LogLevel } from './util/logger';
 
 export const config = readConfig('D:/hyperbot-town-irc/config/bot.conf');
 export const bundle = readBundle('D:/hyperbot-town-irc/config/bundle.conf');
 
 async function start() {
+  Logger.level = LogLevel.Verbose;
+
   await Shell.start();
   const bot = new Bot(config, bundle);
   const cli = new Cli(bot);
--- src/util/logger.ts
+++ src/util/logger.ts
@@ -1,23 +1,40 @@
 import chalk from 'chalk';
 
+export enum LogLevel {
+  Debug,
+  Verbose,
+  Info,
+  Warn,
+  Error,
+  Fatal,
+}
+
 export class Logger {
-  private static log(color: chalk.ChalkFunction, char: string, ctx: string, message: string) {
-    console.log(`${color(`[${char}]`)} [${ctx}]: ${message}`);
+  private static loglvl: LogLevel;
+  private static log(color: chalk.ChalkFunction, char: string, ctx: string, message: string, lvl: LogLevel) {
+    if (lvl >= this.loglvl) console.log(`${color(`[${char}]`)} [${ctx}]: ${message}`);
   }
 
+  static set level(lvl: LogLevel) {
+    this.loglvl = lvl;
+  }
+
+  static debug(ctx: string, message: string) {
+    this.log(chalk.cyanBright, 'D', ctx, message, LogLevel.Debug);
+  }
   static verbose(ctx: string, message: string) {
-    this.log(chalk.cyanBright, 'V', ctx, message);
+    this.log(chalk.cyanBright, 'V', ctx, message, LogLevel.Verbose);
   }
   static info(ctx: string, message: string) {
-    this.log(chalk.blue, 'I', ctx, message);
+    this.log(chalk.blue, 'I', ctx, message, LogLevel.Info);
   }
   static warn(ctx: string, message: string) {
-    this.log(chalk.yellow, 'W', ctx, message);
+    this.log(chalk.yellow, 'W', ctx, message, LogLevel.Warn);
   }
   static error(ctx: string, message: string) {
-    this.log(chalk.redBright, 'E', ctx, message);
+    this.log(chalk.redBright, 'E', ctx, message, LogLevel.Error);
   }
   static fatal(ctx: string, message: string) {
-    this.log(chalk.grey, 'F', ctx, message);
+    this.log(chalk.grey, 'F', ctx, message, LogLevel.Fatal);
   }
 }
