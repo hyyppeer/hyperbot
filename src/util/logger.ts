b7cf495f30164a718795a8d16c350482f257f7aa
Mi2050 <95050519+Mi2050@users.noreply.github.com>
2023-06-24 07:39:27 +0200

    chrome://inspect debugger compatibility

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -21,13 +21,13 @@ export class Logger {
       .map((value) => `${starter}${value}`)
       .join('\n');
   }
-  private static log(color: chalk.ChalkFunction, char: string, ctx: string, message: string, lvl: LogLevel) {
+  private static log(color: chalk.ChalkFunction, char: string, ctx: string, message: string, lvl: LogLevel, outputter: Function) {
     const ts = new Date();
     const tstext = chalk.grey(`${ts.getHours().toString().padStart(2, '0')}:${ts.getMinutes().toString().padStart(2, '0')}`);
     const logchar = color(`-${char}-`);
     const ctxt = color(`${ctx} | `.padStart(13, ' '));
     const starter = `${tstext} ${logchar} ${ctxt}`;
-    if (lvl >= this.loglvl) console.log(this.wrap(starter, message));
+    if (lvl >= this.loglvl) outputter(this.wrap(starter, message));
   }
 
   static set level(lvl: LogLevel) {
@@ -35,21 +35,21 @@ export class Logger {
   }
 
   static debug(ctx: string, message: string) {
-    this.log(chalk.cyanBright, 'D', ctx, message, LogLevel.Debug);
+    this.log(chalk.cyanBright, 'D', ctx, message, LogLevel.Debug, console.debug);
   }
   static verbose(ctx: string, message: string) {
-    this.log(chalk.greenBright, 'V', ctx, message, LogLevel.Verbose);
+    this.log(chalk.greenBright, 'V', ctx, message, LogLevel.Verbose, console.debug);
   }
   static info(ctx: string, message: string) {
-    this.log(chalk.blue, 'I', ctx, message, LogLevel.Info);
+    this.log(chalk.blue, 'I', ctx, message, LogLevel.Info, console.info);
   }
   static warn(ctx: string, message: string) {
-    this.log(chalk.yellow, 'W', ctx, message, LogLevel.Warn);
+    this.log(chalk.yellow, 'W', ctx, message, LogLevel.Warn, console.warn);
   }
   static error(ctx: string, message: string) {
-    this.log(chalk.redBright, 'E', ctx, message, LogLevel.Error);
+    this.log(chalk.redBright, 'E', ctx, message, LogLevel.Error, console.error);
   }
   static fatal(ctx: string, message: string) {
-    this.log(chalk.grey, 'F', ctx, message, LogLevel.Fatal);
+    this.log(chalk.grey, 'F', ctx, message, LogLevel.Fatal, console.error);
   }
 }
