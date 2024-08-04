1f85dbb6e4cd778969793ca8bb54badfb7c6e0d6
hyper <95050519+hyyppeer@users.noreply.github.com>
2024-08-05 02:57:34 +0300

    refactor modules/packages.ts

--- src/modules/packages.ts
+++ src/modules/packages.ts
@@ -1,12 +1,89 @@
 import { writeFileSync } from 'fs';
-import { JsonCmdAction, JsonCmdActionType, JsonCmdListener, JsonCommand, JsonCommands, JsonPackage } from '../util/jsoncmds';
+import {
+  JsonCmdAction,
+  JsonCmdActionType,
+  JsonCmdListener,
+  JsonCommand,
+  JsonCommands,
+  JsonPackage,
+} from '../util/jsoncmds';
 import { Rank } from '../bot/bot';
-import { CommandErrorId, Module, defineCommand, defineModule } from './modules';
+import {
+  CmdApi,
+  CommandErrorId,
+  Module,
+  defineCommand,
+  defineModule,
+} from './modules';
 import { Logger } from '../util/logger';
 import { config } from '..';
 import { Shell } from '../bot/services/town/shell';
 import { randomBytes } from 'crypto';
 import { HyperPackageManager } from '../util/hpm';
+
+function askForAction(cmd: CmdApi): JsonCmdAction {
+  const typea = await cmd.ask(
+    'What is the type of this action? (say/do)',
+    fail
+  );
+  const type: JsonCmdActionType = typea === 'do' ? typea : 'say';
+
+  const argarr: string[] = [];
+  while (true) {
+    if (!(await cmd.confirm('Would you like to add an argument? (y/N)'))) {
+      break;
+    }
+
+    const arg = await cmd.ask('Please enter the argument.', fail);
+    argarr.push(arg);
+  }
+
+  return {
+    type,
+    args: argarr,
+  };
+}
+
+function askForCommand(cmd: CmdApi): JsonCommand {
+  const name = await cmd.ask('What would you like to name this command?', fail);
+  const syntax = await cmd.ask('What is the syntax to use this command?', fail);
+  const help = await cmd.ask('What is the help for this command?', fail);
+  const rank = Number.parseInt(
+    await cmd.ask('What is the minimum rank to use this command? (1-3)', fail)
+  );
+  const listeners: JsonCmdListener[] = [];
+  while (true) {
+    if (!(await cmd.confirm('Would you like to add a listener? (y/N)'))) {
+      break;
+    }
+
+    const onans = await cmd.ask(
+      'When would you like this command to be triggered? (ran)',
+      fail
+    );
+    const on = onans === 'ran' ? onans : 'ran';
+
+    const actions: JsonCmdAction[] = [];
+    while (true) {
+      if (!(await cmd.confirm('Would you like to add an action? (y/N)'))) {
+        break;
+      }
+      actions.push(askForAction(cmd));
+    }
+    listeners.push({
+      on,
+      actions,
+    });
+  }
+
+  return {
+    help,
+    listeners,
+    syntax,
+    name,
+    rankRequired: rank,
+  };
+}
 
 export const packages: Module = defineModule('packages', [
   defineCommand(
@@ -17,64 +94,22 @@ export const packages: Module = defineModule('packages', [
       const fail = () => {
         throw 'timeout';
       };
-      if (cmd.channel.startsWith('#')) throw 'Sorry, createpkg is only allowed in PMs to prevent spam.';
-      cmd.respond('Please note: only ASCII characters are allowed in these fields');
-      const name = await cmd.ask('What would you like to name your package?', fail);
+      if (cmd.channel.startsWith('#'))
+        throw 'Sorry, createpkg is only allowed in PMs to prevent spam.';
+      cmd.respond(
+        'Please note: only ASCII characters are allowed in these fields'
+      );
+      const name = await cmd.ask(
+        'What would you like to name your package?',
+        fail
+      );
       let commands: JsonCommand[] = [];
 
       while (true) {
         if (!(await cmd.confirm('Would you like to add a command? (y/N)'))) {
           break;
         }
-
-        const name = await cmd.ask('What would you like to name this command?', fail);
-        const syntax = await cmd.ask('What is the syntax to use this command?', fail);
-        const help = await cmd.ask('What is the help for this command?', fail);
-        const rank = Number.parseInt(await cmd.ask('What is the minimum rank to use this command? (1-3)', fail));
-        const listeners: JsonCmdListener[] = [];
-        while (true) {
-          if (!(await cmd.confirm('Would you like to add a listener? (y/N)'))) {
-            break;
-          }
-
-          const onans = await cmd.ask('When would you like this command to be triggered? (ran)', fail);
-          const on = onans === 'ran' ? onans : 'ran';
-
-          const actions: JsonCmdAction[] = [];
-          while (true) {
-            if (!(await cmd.confirm('Would you like to add an action? (y/N)'))) {
-              break;
-            }
-
-            const typea = await cmd.ask('What is the type of this action? (say/do)', fail);
-            const type: JsonCmdActionType = typea === 'do' ? typea : 'say';
-
-            const argarr: string[] = [];
-            while (true) {
-              if (!(await cmd.confirm('Would you like to add an argument? (y/N)'))) {
-                break;
-              }
-
-              const arg = await cmd.ask('Please enter the argument.', fail);
-              argarr.push(arg);
-            }
-            actions.push({
-              type,
-              args: argarr,
-            });
-          }
-          listeners.push({
-            on,
-            actions,
-          });
-        }
-        commands.push({
-          help,
-          listeners,
-          syntax,
-          name,
-          rankRequired: rank,
-        });
+        commands.push(askForCommand(cmd));
       }
 
       const pkg: JsonPackage = {
@@ -87,22 +122,40 @@ export const packages: Module = defineModule('packages', [
       const pkgid = randomBytes(8).toString('hex');
 
       await Shell.exec(`echo ${pkgstr} > /home/hyper/hyperbot/pkgs/${pkgid}`);
-      cmd.respond(`Your package has been stored in /home/hyper/hyperbot/pkgs/${pkgid}`);
+      cmd.respond(
+        `Your package has been stored in /home/hyper/hyperbot/pkgs/${pkgid}`
+      );
       cmd.respond('Load it using the loadpkgstr command');
-      const submit = await cmd.confirm('Would you like to submit this package to be reviewed and potentially added to the bot?');
-      const hpmSubmit = await cmd.confirm('Would you like to submit this package to HPM (hyper package manager)');
+      const submit = await cmd.confirm(
+        'Would you like to submit this package to be reviewed and potentially added to the bot?'
+      );
+      const hpmSubmit = await cmd.confirm(
+        'Would you like to submit this package to HPM (hyper package manager)'
+      );
 
       if (submit) {
-        writeFileSync(`${config.jsonpkg.submissionrootdir}/submit-${pkgid}.pkg`, pkgstr);
-        writeFileSync(`${config.jsonpkg.submissionrootdir}/submit-${pkgid}.json`, json);
+        writeFileSync(
+          `${config.jsonpkg.submissionrootdir}/submit-${pkgid}.pkg`,
+          pkgstr
+        );
+        writeFileSync(
+          `${config.jsonpkg.submissionrootdir}/submit-${pkgid}.json`,
+          json
+        );
         cmd.respond('Submitted!');
-        Logger.info('Package', `Submitted package ${pkgid} for review in ${config.jsonpkg.submissionrootdir}`);
+        Logger.info(
+          'Package',
+          `Submitted package ${pkgid} for review in ${config.jsonpkg.submissionrootdir}`
+        );
       } else cmd.respond('Your package has not been submitted');
       if (hpmSubmit) {
         const user = await cmd.identify();
-        const name = await cmd.ask('Please enter a name for your HPM package', () => {
-          throw 'Your package has not been submitted to HPM';
-        });
+        const name = await cmd.ask(
+          'Please enter a name for your HPM package',
+          () => {
+            throw 'Your package has not been submitted to HPM';
+          }
+        );
         HyperPackageManager.add(`~${user}/${name}`, pkgstr);
         cmd.respond('Submitted to HPM!');
       } else cmd.respond('Your package has not been submitted to HPM');
